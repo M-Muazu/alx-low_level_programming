aa8ee834de9237d41654781ed593a63b0b969734
M-Muazu <shamsuddeenmuazu11@yahoo.com>
2023-02-16 15:12:42 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Used the funtion printf to print
+ * Return: 0 (Succesful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
