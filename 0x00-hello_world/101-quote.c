5ae18dc16fc29bf2883da2b11401abb81025efee
M-Muazu <shamsuddeenmuazu11@yahoo.com>
2023-02-16 16:44:00 -0800

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that print standart error
+ * main - A c program that print standart error
  * Return 1 (Succesful)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
