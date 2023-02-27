d813f22901a8aa9ff5e57251c8d54e341b9474c8
M-Muazu <shamsuddeenmuazu11@yahoo.com>
2023-02-27 14:30:24 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- *_strlen - return the length of a string
+ * _strlen - returns the length of a string
  * @s: string
  * Return: length
  */
-
-int_string(char *s)
+int _strlen(char *s)
 {
-	int longi = 0
+	int longi = 0;
 
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
+
 	return (longi);
 }
+
